f51a38046bdfac52d6ba1f4205f0518468fe02b0
Omkar Yadav <154461228+OmkarYadav1089@users.noreply.github.com>
2024-10-22 01:25:00 +0530

    Added logic to use 'an' for careers starting with vowels in index.js

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-// const containerEl = document.querySelector('.container');
-
-// const careers = ['Web Developer',
-//   'Trader','Shayar','Investor'
-// ];
-// let careerIndex = 0;
-
-
-// let characterIndex = 0;   
-
-// updateText();
-// function updateText(){
-//   characterIndex++;
-//   containerEl.innerHTML=`<h1>I am ${careers[careerIndex].slice(0,1)==='I'? "an" : "a"} ${careers[careerIndex].slice(0, characterIndex)}</h1>`;
- 
-//   if(characterIndex===careers[careerIndex].length){
-//     careerIndex++
-//     characterIndex=0;
-//   }
-//   if(careerIndex===careers.length){
-//     careerIndex=0;
-//   }
-//   setTimeout(updateText,200)
-// }
-
-
 const containerEl = document.querySelector('.container');
 
 const careers = ['Web Developer', 'Trader', 'Shayar', 'Investor','Astrologer'];
